7f20f013a28226dcc00a7c20e37fa08c1385a517
Daniel <djvalderrama67@ucatolica.edu.co>
2022-11-16 15:44:05 -0500

    Informacion de lasgorras

---
+++ src/components/common/gorraData.js
@@ -0,0 +1,33 @@
+
+//     const Lista [
+//         {
+//         imagen:'gorra_adidas_gris.png',
+//         titulo: 'Gorra Adidas Gris Oscuro',
+//         descripcion:'Gorra ADIDAS de la mejor calidad color gris oscuro y con excelente materiales de fabricacion',
+//         precio:119900,
+//     },
+//     {
+//         imagen:'gorra_adidas_negro.png',
+//         titulo: 'Gorra Adidas Negro',
+//         descripcion:'Gorra ADIDAS de la mejor calidad color negro con los mejores materiales de fabricacion',
+//         precio:199900,
+//     },
+//     {
+//         imagen:'gorra_creeper_verde.png',
+//         titulo: 'Gorra Creeper color verde',
+//         descripcion:'Gorra con cara de Creeper(minecraft) color verde con excelente calidad',
+//         precio:79900,
+//     },
+//     {
+//         imagen:'gorra_gris.png',
+//         titulo: 'Gorra color gris',
+//         descripcion:'Gorra de color gris con los mejores materiales de fabricacion, brindando una comodidad excelente',
+//         precio:69900,
+//     },
+//     {
+//         imagen:'gorra_LA_azul.png',
+//         titulo: 'Gorra LA color azul oscuro',
+//         descripcion:'Gorra LA color azul oscuro de excelente calidad',
+//         precio:69900,
+//     },
+// ]
